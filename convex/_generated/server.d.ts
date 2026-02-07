b52f17723b1f250af171b858bdced562bfef881e
Cole <brownecole@gmail.com>
2026-02-07 15:36:25 -0500

    chore: add Convex API stubs for build without Convex config

---
+++ convex/_generated/server.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/**
+ * Generated Convex server types stub
+ */
+
+import type { GenericQueryCtx, GenericMutationCtx, GenericActionCtx } from "convex/server";
+import type { DataModel } from "./dataModel";
+
+export type QueryCtx = GenericQueryCtx<DataModel>;
+export type MutationCtx = GenericMutationCtx<DataModel>;
+export type ActionCtx = GenericActionCtx<DataModel>;
