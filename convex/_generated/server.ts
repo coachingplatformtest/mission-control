739bce05ee0612d1c4875290c02ced8f98169ef4
Cole <brownecole@gmail.com>
2026-02-07 16:06:16 -0500

    fix: add server.ts stub

---
+++ convex/_generated/server.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+// @ts-nocheck
+/**
+ * Convex server stub - will be replaced when Convex is configured
+ */
+
+// Stub implementations that do nothing
+export const query = (config: any) => config;
+export const mutation = (config: any) => config;
+export const action = (config: any) => config;
+export const internalQuery = (config: any) => config;
+export const internalMutation = (config: any) => config;
+export const internalAction = (config: any) => config;
